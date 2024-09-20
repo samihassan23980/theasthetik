406485d4613872668c6f04d1321b871a70a3c0c8
samihassan23980 <samisami23980@gmail.com>
2024-09-20 21:25:49 +0500

    add analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
-
+import { GoogleAnalytics } from "@next/third-parties/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -13,21 +13,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <head>
-      {/* Favicon for all browsers */}
-      <link 
-        rel="icon" 
-        href="https://firebasestorage.googleapis.com/v0/b/the-asthetik.appspot.com/o/the-ashthetik%2Fshorticon.png?alt=media&token=b572e6aa-12f1-4435-bdc1-a2ab650c3b34" 
-        type="image/png" 
-        sizes="32x32" 
-      />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </head>
-    <body className={inter.className}>
-      <Navbar />
-      {children}
-      <Footer />
-    </body>
-  </html>
+      <head>
+        {/* Favicon for all browsers */}
+        <link
+          rel="icon"
+          href="https://firebasestorage.googleapis.com/v0/b/the-asthetik.appspot.com/o/the-ashthetik%2Fshorticon.png?alt=media&token=b572e6aa-12f1-4435-bdc1-a2ab650c3b34"
+          type="image/png"
+          sizes="32x32"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body className={inter.className}>
+        <GoogleAnalytics gaId="G-CX6NN679QS" />
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
   );
 }
